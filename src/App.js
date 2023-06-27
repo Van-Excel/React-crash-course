65767ecdea9e22449857445f53e98e56e63bb003
Van-Excel <66915264+Van-Excel@users.noreply.github.com>
2023-06-27 10:55:26 +0100

    Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './style.css';
 import { FirstComponent } from './FirstComponent.js';
+import {Props} from "./Props.js"
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
       <FirstComponent />
+      <Props name= 'vanexcel' />
+      <Props name= 'Theophilus'><p>This is a child prop</p></Props>
     </div>
   );
 }
